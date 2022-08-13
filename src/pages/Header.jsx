3fcdaa174b6b4ccad431e1af12f61ea7a2f3a65a
SeoNaru <tjehddnr12345@naver.com>
2022-08-13 13:50:00 +0900

    from창 css
    from창 css

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,7 +6,19 @@ const header = () => {
     <div className="header">
       <div className="inner">
         <a href="/" className="logo">
-          <img src="https://img.icons8.com/ios-filled/60/000000/pineapple.png" alt="PineApple" />
+          <img
+            src="https://img.icons8.com/ios-filled/40/000000/pineapple.png"
+            alt="PineApple"
+          />
+        </a>
+        <div className="menu"></div>
+        <div className="iMacCategories">iMac</div>
+        <div className="iPhoneCategories">iPhone</div>
+        <a href="/Login" className="loginlogo">
+        <img src="https://img.icons8.com/external-flat-icons-inmotus-design/67/000000/external-login-antivirus-flat-icons-inmotus-design.png" alt=""/>
+        </a>
+        <a href="/Basket" className="shoppingLogo">
+        <img src="https://img.icons8.com/ios-glyphs/60/000000/shopping-bag.png" alt=""/>
         </a>
       </div>
     </div>
